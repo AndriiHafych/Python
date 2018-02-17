30dbc99b0677f2b9e3e9c59e5311aae868ed5d68
AndriiHafych <33121339+AndriiHafych@users.noreply.github.com>
2018-02-17 13:01:33 +0200

    Update field of driems.py

--- field of driems.py
+++ field of driems.py
@@ -5,7 +5,7 @@ while ''.join(under_) != const:
     guess = input('try')
     while guess in guess_word:
         under_[guess_word.find(guess)] = guess
-        kostil = list(guess_word)   # Заміна відгаданої букви на _ що б знайти повтор.
+        kostil = list(guess_word)
         kostil[guess_word.find(guess)] = '_'
         guess_word = ''.join(kostil)
     print(under_)
